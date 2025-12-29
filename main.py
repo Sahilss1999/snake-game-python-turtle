33146c473243f07d93308ca538c0f1fd67326113
Sahil Shetye <shetyesahil1999@gmail.com>
2025-12-29 18:32:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+import turtle
+from turtle import Screen, Turtle
+from snake import Snake
+from food import Food
+from scoreboard import SCOREBOARD
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = SCOREBOARD()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+# create tuples in list to create snake starting positon
+
+
+
+
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or  snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # detect collision with own
+    for segment in snake.segments[1:]:
+         if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+
+
+
+
+
+
+screen.exitonclick()
